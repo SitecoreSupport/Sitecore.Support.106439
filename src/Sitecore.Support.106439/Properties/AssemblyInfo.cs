1e005b6855ee9c722b19bcfbac0e27b42f0e034e
Dmitry Kolinchuk <dmko@sitecore.net>
2016-12-22 13:04:24 +0200

    Fix the issue with loading items in the “Multilist with Search” field when using the “Field Editor” dialog box from “Experience Editor” or “User Manager”

---
+++ src/Sitecore.Support.106439/Properties/AssemblyInfo.cs
@@ -0,0 +1,6 @@
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Sitecore.Support.106439")]
+[assembly: AssemblyProduct("Sitecore.Support.106439")]
+[assembly: ComVisible(false)]
